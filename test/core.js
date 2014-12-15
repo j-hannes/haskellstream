f854a8f90cd34b6c5ca50e58cf9435528f27a1d1
J-Hannes <Hannes_E@gmx.de>
2014-12-15 18:47:25 +0100

    Add min

--- test/core.js
+++ test/core.js
@@ -3,9 +3,14 @@ var expect = require('chai').expect;
 var core = require('../lib/core.js');
 
 describe('core', function() {
-  describe('succ', function() {
-    it('returns 9 for 8', function() {
+  describe('succ(8)', function() {
+    it('returns 9', function() {
       expect(core.succ(8)).to.be.equal(9);
+    });
+  });
+  describe('min(9,10)', function() {
+    it('returns 9', function() {
+      expect(core.min(9,10)).to.be.equal(9);
     });
   });
 });
